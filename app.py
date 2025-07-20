25289717ee7490fe3de5ef910cabed7866d22086
LightningFryer <91123163+LightningFryer@users.noreply.github.com>
2025-07-20 16:53:30 +0530

    added test url to origins

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://mfp0bs67-3000.inc1.devtunnels.ms"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
