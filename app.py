50750f2a267a990392ccc026257bae105a139ca8
LightningFryer <91123163+LightningFryer@users.noreply.github.com>
2025-07-20 23:15:23 +0530

    shifted CORS back to fastapi version

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from torchvision import transforms
 from PIL import Image
 import torch
 import io
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from model import EnglishCharacterClassifier
 
 app = FastAPI()
